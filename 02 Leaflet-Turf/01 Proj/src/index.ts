d9e15004bcab27d965be483429c1941ff95b3934
Michal Radomski <michalra@op.pl>
2023-05-10 02:46:16 +0200

    Course - commit 02

--- 02 Leaflet-Turf/01 Proj/src/index.ts
+++ 02 Leaflet-Turf/01 Proj/src/index.ts
@@ -9,13 +9,37 @@ let map: L.Map;
 let lyrOSM: L.TileLayer;
 
 $(document).ready(function () {
-  map = L.map("mapDiv", { center: [54.3475, 18.645278], zoom: 13 });
+  map = L.map("mapDiv", {
+    center: [54.3475, 18.645278],
+    zoom: 13,
+    zoomControl: true,
+    dragging: true,
+    minZoom: 4,
+    maxZoom: 18,
+  });
 
   lyrOSM = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    maxZoom: 19,
+    maxZoom: 18,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
   });
 
   map.addLayer(lyrOSM);
   map.addLayer(circle);
+
+  map.on("click", function (event: L.LeafletMouseEvent) {
+    // console.log("event:", event);
+    // console.log("event.latlng.toString():", event.latlng.toString());
+    if (event.originalEvent.shiftKey) {
+      console.log("map.getZoom():", map.getZoom());
+    } else {
+      console.log("event.latlng:", event.latlng);
+    }
+  });
+
+  map.on("contextmenu", function (event: L.LeafletMouseEvent) {
+    const currentTime = new Date();
+    L.marker(event.latlng)
+      .addTo(map)
+      .bindPopup(event.latlng.toString() + "<br/>" + currentTime.toString());
+  });
 });
