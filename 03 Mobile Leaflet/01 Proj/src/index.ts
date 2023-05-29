480f19bdcd65fdc7706da66184ea71efcb4ea447
Michal Radomski <michalra@op.pl>
2023-05-30 01:47:59 +0200

    Providers plugin was added

--- 03 Mobile Leaflet/01 Proj/src/index.ts
+++ 03 Mobile Leaflet/01 Proj/src/index.ts
@@ -9,12 +9,39 @@ const map = L.map("mapDiv", {
   attributionControl: true,
 });
 
-L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-}).addTo(map);
-
 new L.Control.Zoom({ position: "topright" }).addTo(map);
 
+// Map Providers
+//* V1
+// L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+//   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+// }).addTo(map);
+
+//* V2
+const lyrOSM = L.tileLayer
+  // @ts-ignore
+  .provider("OpenStreetMap.Mapnik", {
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  });
+map.addLayer(lyrOSM);
+
+// @ts-ignore
+const lyrWaterColor = L.tileLayer.provider("Stamen.Watercolor");
+// @ts-ignore
+const lyrTopo = L.tileLayer.provider("OpenTopoMap");
+// @ts-ignore
+const lyrImagery = L.tileLayer.provider("Esri.WorldImagery");
+
+const objBaseMaps = {
+  "Open Street Maps": lyrOSM,
+  "Topo Maps": lyrTopo,
+  Imagery: lyrImagery,
+  Watercolor: lyrWaterColor,
+};
+
+L.control.layers(objBaseMaps).addTo(map);
+
+// Location
 // map.locate({ setView: true, maxZoom: 16 });
 // function onLocationFound(e: { accuracy: number; latlng: L.LatLngExpression }) {
 //   var radius = e.accuracy;
@@ -28,6 +55,7 @@ new L.Control.Zoom({ position: "topright" }).addTo(map);
 // }
 // map.on("locationfound", onLocationFound);
 
+// Measure Plugin
 const measureOptions = {
   position: "topright",
   unit: "kilometres",
