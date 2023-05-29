cbab719c0650d6e6508883c3b6bbd77c860c9e5f
Michal Radomski <michalra@op.pl>
2023-05-30 01:04:37 +0200

    Map - initial setup

--- 03 Mobile Leaflet/01 Proj/src/index.ts
+++ 03 Mobile Leaflet/01 Proj/src/index.ts
@@ -1,1 +1,14 @@
+// Initialize map
+const map = L.map("mapDiv", {
+  center: [54.3475, 18.645278],
+  zoom: 13,
+  zoomControl: true,
+  dragging: true,
+  minZoom: 4,
+  maxZoom: 18,
+  attributionControl: true,
+});
 
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
