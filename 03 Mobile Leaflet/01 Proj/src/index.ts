76ca9f4e1f78dd7ac7ce171641e4841960bd48ee
Michal Radomski <michalra@op.pl>
2023-05-30 01:10:24 +0200

    Location was added

--- 03 Mobile Leaflet/01 Proj/src/index.ts
+++ 03 Mobile Leaflet/01 Proj/src/index.ts
@@ -12,3 +12,16 @@ const map = L.map("mapDiv", {
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
+
+// map.locate({ setView: true, maxZoom: 16 });
+// function onLocationFound(e: { accuracy: number; latlng: L.LatLngExpression }) {
+//   var radius = e.accuracy;
+
+//   L.marker(e.latlng)
+//     .addTo(map)
+//     .bindPopup("You are within " + radius + " meters from this point")
+//     .openPopup();
+
+//   L.circle(e.latlng, radius).addTo(map);
+// }
+// map.on("locationfound", onLocationFound);
