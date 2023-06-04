89b3512452003039197b3217bbbaa314b589acab
Michal Radomski <michalra@op.pl>
2023-06-04 02:41:52 +0200

    Course - commit5

--- 04 ChartsJS/01 Proj/src/index.ts
+++ 04 ChartsJS/01 Proj/src/index.ts
@@ -61,7 +61,57 @@ const { Chart } = window;
 // console.log("ctx:", ctx);
 
 //* Line Chart
-// Data block
+// // Data block
+// const labels = ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"];
+
+// const data = {
+//   labels: labels,
+//   datasets: [
+//     {
+//       label: "# of Votes",
+//       data: [12, 19, 3, 5, 2, 3],
+//       fill: false,
+//       backgroundColor: [
+//         "rgba(255, 99, 132, 0.2)",
+//         "rgba(54, 162, 235, 0.2)",
+//         "rgba(255, 206, 86, 0.2)",
+//         "rgba(75, 192, 192, 0.2)",
+//         "rgba(153, 102, 255, 0.2)",
+//         "rgba(255, 159, 64, 0.2)",
+//       ],
+//       borderColor: [
+//         "rgba(255, 99, 132, 1)",
+//         "rgba(54, 162, 235, 1)",
+//         "rgba(255, 206, 86, 1)",
+//         "rgba(75, 192, 192, 1)",
+//         "rgba(153, 102, 255, 1)",
+//         "rgba(255, 159, 64, 1)",
+//       ],
+//       tension: 0.4,
+//       borderWidth: 5,
+//       // pointRadius: 0,
+//       // pointHoverRadius: 0,
+//     },
+//   ],
+// };
+
+// // Config block
+// const config = {
+//   type: "line",
+//   data: data,
+//   options: {
+//     scales: {
+//       y: {
+//         beginAtZero: true,
+//       },
+//     },
+//   },
+// };
+
+// // Init render block
+// new Chart(document.getElementById("myChart") as HTMLCanvasElement, config as ChartConfiguration);
+
+//* Horizontal Line Chart
 const labels = ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"];
 
 const data = {
@@ -70,7 +120,6 @@ const data = {
     {
       label: "# of Votes",
       data: [12, 19, 3, 5, 2, 3],
-      fill: false,
       backgroundColor: [
         "rgba(255, 99, 132, 0.2)",
         "rgba(54, 162, 235, 0.2)",
@@ -87,19 +136,16 @@ const data = {
         "rgba(153, 102, 255, 1)",
         "rgba(255, 159, 64, 1)",
       ],
-      tension: 0.4,
-      borderWidth: 5,
-      // pointRadius: 0,
-      // pointHoverRadius: 0,
+      borderWidth: 3,
     },
   ],
 };
 
-// Config block
 const config = {
-  type: "line",
+  type: "bar",
   data: data,
   options: {
+    indexAxis: "y",
     scales: {
       y: {
         beginAtZero: true,
@@ -108,5 +154,4 @@ const config = {
   },
 };
 
-// Init render block
-const myChart: Chart = new Chart(document.getElementById("myChart") as HTMLCanvasElement, config as ChartConfiguration);
+new Chart(document.getElementById("myChart") as HTMLCanvasElement, config as ChartConfiguration);
