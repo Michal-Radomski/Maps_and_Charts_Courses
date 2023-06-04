1ed13154031c6cd30ca2fcdca6f2fff706a68bf1
Michal Radomski <michalra@op.pl>
2023-06-04 02:35:49 +0200

    Course - commit4

--- 04 ChartsJS/01 Proj/src/index.ts
+++ 04 ChartsJS/01 Proj/src/index.ts
@@ -4,13 +4,65 @@ const { Chart } = window;
 // console.log("Chart:", Chart);
 // console.log({ window });
 
+//* Bar Chart
+// // Data block
+// const labels = ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"];
+// labels.shift();
+// labels.unshift("Red2");
+
+// labels.pop();
+// labels.push("Orange2");
+
+// const data = {
+//   labels: labels,
+//   datasets: [
+//     {
+//       label: "# of Votes",
+//       data: [12, 19, 3, 5, 2, 3],
+//       backgroundColor: [
+//         "rgba(255, 99, 132, 0.2)",
+//         "rgba(54, 162, 235, 0.2)",
+//         "rgba(255, 206, 86, 0.2)",
+//         "rgba(75, 192, 192, 0.2)",
+//         "rgba(153, 102, 255, 0.2)",
+//         "rgba(255, 159, 64, 0.2)",
+//       ],
+//       borderColor: [
+//         "rgba(255, 99, 132, 1)",
+//         "rgba(54, 162, 235, 1)",
+//         "rgba(255, 206, 86, 1)",
+//         "rgba(75, 192, 192, 1)",
+//         "rgba(153, 102, 255, 1)",
+//         "rgba(255, 159, 64, 1)",
+//       ],
+//       borderWidth: 1,
+//     },
+//   ],
+// };
+
+// // Config block
+// const config = {
+//   type: "bar",
+//   data: data,
+//   options: {
+//     scales: {
+//       y: {
+//         beginAtZero: true,
+//       },
+//     },
+//   },
+// };
+
+// // Init render block
+// const myChart: Chart = new Chart(document.getElementById("myChart") as HTMLCanvasElement, config as ChartConfiguration);
+// console.log("myChart:", myChart);
+
+// const ctx = (document.getElementById("myChart") as HTMLCanvasElement)?.getContext("2d");
+// console.log("ctx:", ctx);
+
+//* Line Chart
 // Data block
 const labels = ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"];
-labels.shift();
-labels.unshift("Red2");
-
-labels.pop();
-labels.push("Orange2");
 
 const data = {
   labels: labels,
@@ -18,6 +70,7 @@ const data = {
     {
       label: "# of Votes",
       data: [12, 19, 3, 5, 2, 3],
+      fill: false,
       backgroundColor: [
         "rgba(255, 99, 132, 0.2)",
         "rgba(54, 162, 235, 0.2)",
@@ -34,14 +87,17 @@ const data = {
         "rgba(153, 102, 255, 1)",
         "rgba(255, 159, 64, 1)",
       ],
-      borderWidth: 1,
+      tension: 0.4,
+      borderWidth: 5,
+      // pointRadius: 0,
+      // pointHoverRadius: 0,
     },
   ],
 };
 
 // Config block
 const config = {
-  type: "bar",
+  type: "line",
   data: data,
   options: {
     scales: {
@@ -54,7 +110,3 @@ const config = {
 
 // Init render block
 const myChart: Chart = new Chart(document.getElementById("myChart") as HTMLCanvasElement, config as ChartConfiguration);
-console.log("myChart:", myChart);
-
-// const ctx = (document.getElementById("myChart") as HTMLCanvasElement)?.getContext("2d");
-// console.log("ctx:", ctx);
