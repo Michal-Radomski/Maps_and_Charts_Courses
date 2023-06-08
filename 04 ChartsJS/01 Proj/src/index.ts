323035e15d6917fa35c53d9037683bc0cc5f053e
Michal Radomski <michalra@op.pl>
2023-06-09 01:23:15 +0200

    Course - commit25

--- 04 ChartsJS/01 Proj/src/index.ts
+++ 04 ChartsJS/01 Proj/src/index.ts
@@ -856,31 +856,94 @@ const { Chart } = window;
 //   },
 // };
 
-//* Bar Chart - change color of single grid line
+// //* Bar Chart - change color of single grid line
+// const labels = ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"];
+
+// const data = {
+//   labels: labels,
+//   datasets: [
+//     {
+//       label: "# of Votes",
+//       data: [12, 19, 3, 5, 2, 3],
+//       backgroundColor: [
+//         "rgba(255, 99, 132, 0.2)",
+//         "rgba(54, 162, 235, 0.2)",
+//         "rgba(255, 206, 86, 0.2)",
+//         "rgba(75, 192, 192, 0.2)",
+//         "rgba(153, 102, 255, 0.2)",
+//         "rgba(255, 159, 64, 0.2)",
+//       ],
+//       borderColor: [
+//         "rgba(255, 99, 132, 1)",
+//         "rgba(54, 162, 235, 1)",
+//         "rgba(255, 206, 86, 1)",
+//         "rgba(75, 192, 192, 1)",
+//         "rgba(153, 102, 255, 1)",
+//         "rgba(255, 159, 64, 1)",
+//       ],
+//       borderWidth: 3,
+//     },
+//   ],
+// };
+
+// const config = {
+//   type: "bar",
+//   data: data,
+//   options: {
+//     scales: {
+//       x: {
+//         grid: {
+//           color: (context: { index: number }) => {
+//             // console.log(context.index);
+//             // console.log({ context });
+//             if (context.index === 3) {
+//               return "rgba(75, 192, 192, 1)";
+//             } else if (context.index === 4) {
+//               return "darkMagenta";
+//             } else {
+//               return "rgba(0, 0, 0, 0.1)";
+//             }
+//           },
+//         },
+//       },
+//       y: {
+//         beginAtZero: true,
+//         grid: {
+//           color: (context: { index: number }) => {
+//             // console.log(context.index);
+//             // console.log({ context });
+//             if (context.index === 5) {
+//               return "rgba(75, 192, 192, 1)";
+//             } else if (context.index === 4) {
+//               return "darkMagenta";
+//             } else {
+//               return "rgba(0, 0, 0, 0.1)";
+//             }
+//           },
+//         },
+//       },
+//     },
+//   },
+// };
+
+//* Bar Chart - floating / range chart
 const labels = ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"];
 
 const data = {
   labels: labels,
   datasets: [
     {
-      label: "# of Votes",
-      data: [12, 19, 3, 5, 2, 3],
-      backgroundColor: [
-        "rgba(255, 99, 132, 0.2)",
-        "rgba(54, 162, 235, 0.2)",
-        "rgba(255, 206, 86, 0.2)",
-        "rgba(75, 192, 192, 0.2)",
-        "rgba(153, 102, 255, 0.2)",
-        "rgba(255, 159, 64, 0.2)",
+      label: "Temperature in C",
+      data: [
+        [8, 12],
+        [9, 16],
+        [6, 9],
+        [4, 10],
+        [3, 12],
+        [9, 2],
       ],
-      borderColor: [
-        "rgba(255, 99, 132, 1)",
-        "rgba(54, 162, 235, 1)",
-        "rgba(255, 206, 86, 1)",
-        "rgba(75, 192, 192, 1)",
-        "rgba(153, 102, 255, 1)",
-        "rgba(255, 159, 64, 1)",
-      ],
+      backgroundColor: ["rgba(255, 99, 132, 0.2)"],
+      borderColor: ["rgba(255, 99, 132, 1)"],
       borderWidth: 3,
     },
   ],
@@ -890,37 +953,11 @@ const config = {
   type: "bar",
   data: data,
   options: {
+    borderSkipped: false,
+    indexAxis: "y",
     scales: {
-      x: {
-        grid: {
-          color: (context: { index: number }) => {
-            // console.log(context.index);
-            // console.log({ context });
-            if (context.index === 3) {
-              return "rgba(75, 192, 192, 1)";
-            } else if (context.index === 4) {
-              return "darkMagenta";
-            } else {
-              return "rgba(0, 0, 0, 0.1)";
-            }
-          },
-        },
-      },
       y: {
         beginAtZero: true,
-        grid: {
-          color: (context: { index: number }) => {
-            // console.log(context.index);
-            // console.log({ context });
-            if (context.index === 5) {
-              return "rgba(75, 192, 192, 1)";
-            } else if (context.index === 4) {
-              return "darkMagenta";
-            } else {
-              return "rgba(0, 0, 0, 0.1)";
-            }
-          },
-        },
       },
     },
   },
