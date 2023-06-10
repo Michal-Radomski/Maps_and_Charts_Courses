6571120cbe4f0c578e3204714476f107648cee0c
Michal Radomski <michalra@op.pl>
2023-06-10 22:45:38 +0200

    Course - commit31

--- 04 ChartsJS/01 Proj/src/index.ts
+++ 04 ChartsJS/01 Proj/src/index.ts
@@ -1151,52 +1151,93 @@ const { Chart } = window;
 // };
 
 //*  Dotted Dashed Line Chart
-const color = "Yellow";
-const colorLabels = ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"];
-const dash = (ctx: { p0DataIndex: number }, value: number[]) => {
-  // console.log({ ctx, value });
-  return ctx.p0DataIndex < colorLabels.indexOf(color) ? value : [6, 0];
-};
+// const color = "Yellow";
+// const colorLabels = ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"];
+// const dash = (ctx: { p0DataIndex: number }, value: number[]) => {
+//   // console.log({ ctx, value });
+//   return ctx.p0DataIndex < colorLabels.indexOf(color) ? value : [6, 0];
+// };
 
+// const data = {
+//   labels: colorLabels,
+//   datasets: [
+//     {
+//       label: "# of Votes",
+//       data: [12, 19, 3, 5, 2, 3],
+//       backgroundColor: [
+//         "rgba(255, 99, 132, 0.2)",
+//         "rgba(54, 162, 235, 0.2)",
+//         "rgba(255, 206, 86, 0.2)",
+//         "rgba(75, 192, 192, 0.2)",
+//         "rgba(153, 102, 255, 0.2)",
+//         "rgba(255, 159, 64, 0.2)",
+//       ],
+//       borderColor: [
+//         "rgba(255, 99, 132, 1)",
+//         "rgba(54, 162, 235, 1)",
+//         "rgba(255, 206, 86, 1)",
+//         "rgba(75, 192, 192, 1)",
+//         "rgba(153, 102, 255, 1)",
+//         "rgba(255, 159, 64, 1)",
+//       ],
+//       tension: 0.4,
+//       borderWidth: 5,
+//       segment: {
+//         //borderDash: [6, 6]
+//         borderDash: (ctx: { p0DataIndex: number }) => dash(ctx, [10, 6]) || [6, 0], //* 10px line, 6px break!!!
+//       },
+//     },
+//   ],
+// };
+
+// const config = {
+//   type: "line",
+//   data: data,
+//   options: {
+//     scales: {
+//       y: {
+//         beginAtZero: true,
+//       },
+//     },
+//   },
+// };
+
+//* Bar Chart - remove spaces
 const data = {
-  labels: colorLabels,
+  labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
   datasets: [
     {
       label: "# of Votes",
       data: [12, 19, 3, 5, 2, 3],
-      backgroundColor: [
-        "rgba(255, 99, 132, 0.2)",
-        "rgba(54, 162, 235, 0.2)",
-        "rgba(255, 206, 86, 0.2)",
-        "rgba(75, 192, 192, 0.2)",
-        "rgba(153, 102, 255, 0.2)",
-        "rgba(255, 159, 64, 0.2)",
-      ],
-      borderColor: [
-        "rgba(255, 99, 132, 1)",
-        "rgba(54, 162, 235, 1)",
-        "rgba(255, 206, 86, 1)",
-        "rgba(75, 192, 192, 1)",
-        "rgba(153, 102, 255, 1)",
-        "rgba(255, 159, 64, 1)",
-      ],
-      tension: 0.4,
-      borderWidth: 5,
-      segment: {
-        //borderDash: [6, 6]
-        borderDash: (ctx: { p0DataIndex: number }) => dash(ctx, [10, 6]) || [6, 0], //* 10px line, 6px break!!!
-      },
+      backgroundColor: ["rgba(255, 159, 64, 0.2)"],
+      borderColor: ["rgba(255, 159, 64, 1)"],
+      borderWidth: 3,
+    },
+    {
+      label: "# of Votes2",
+      data: [12, 19, 3, 5, 2, 3],
+      backgroundColor: ["rgba(255, 99, 132, 0.2)"],
+      borderColor: ["rgba(255, 99, 132, 1)"],
+      borderWidth: 3,
     },
   ],
 };
 
 const config = {
-  type: "line",
+  type: "bar",
   data: data,
   options: {
+    // categoryPercentage: 0.8, // default
+    categoryPercentage: 1, // default
+    barPercentage: 1,
     scales: {
       y: {
         beginAtZero: true,
+      },
+    },
+    plugins: {
+      tooltip: {
+        enabled: false,
       },
     },
   },
