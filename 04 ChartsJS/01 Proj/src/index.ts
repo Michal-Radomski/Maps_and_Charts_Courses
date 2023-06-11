127b2d829ca3abe397ef66a4731def80a5455784
Michal Radomski <michalra@op.pl>
2023-06-11 22:36:06 +0200

    Course - commit37

--- 04 ChartsJS/01 Proj/src/index.ts
+++ 04 ChartsJS/01 Proj/src/index.ts
@@ -1475,12 +1475,91 @@ const { Chart } = window;
 // };
 
 //* chartArea
+// const data = {
+//   labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+//   datasets: [
+//     {
+//       label: "# of Votes",
+//       data: [12, 19, 3, 5, 2, 3],
+//       backgroundColor: [
+//         "rgba(255, 99, 132, 0.2)",
+//         "rgba(54, 162, 235, 0.2)",
+//         "rgba(255, 206, 86, 0.2)",
+//         "rgba(75, 192, 192, 0.2)",
+//         "rgba(153, 102, 255, 0.2)",
+//         "rgba(255, 159, 64, 0.2)",
+//       ],
+//       borderColor: [
+//         "rgba(255, 99, 132, 1)",
+//         "rgba(54, 162, 235, 1)",
+//         "rgba(255, 206, 86, 1)",
+//         "rgba(75, 192, 192, 1)",
+//         "rgba(153, 102, 255, 1)",
+//         "rgba(255, 159, 64, 1)",
+//       ],
+//       borderWidth: 1,
+//     },
+//   ],
+// };
+
+// const chartAreaBorder = {
+//   id: "chartAreaBorder",
+//   beforeDraw(
+//     chart: {
+//       ctx: any;
+//       chartArea: { top: number; bottom: number; left: number; right: number; width: number; height: number };
+//     },
+//     _args: any,
+//     _options: any
+//   ) {
+//     const {
+//       ctx,
+//       chartArea: { top, bottom, left, right, width, height },
+//     } = chart;
+
+//     // console.log({ top });
+//     // console.log({ bottom });
+//     // console.log({ _args });
+//     // console.log({ _options });
+
+//     ctx.strokeStyle = "black";
+//     ctx.strokeRect(right - 1, top, 0, height);
+//     ctx.strokeStyle = "red";
+//     ctx.strokeRect(left, top, width - 1, 0);
+//   },
+// };
+
+// const config = {
+//   type: "bar",
+//   data: data,
+//   options: {
+//     scales: {
+//       y: {
+//         beginAtZero: true,
+//       },
+//     },
+//   },
+//   plugins: [chartAreaBorder],
+// };
+
+//* Waterfall Chart
+const labels = ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"];
+
 const data = {
-  labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+  labels: labels,
   datasets: [
     {
       label: "# of Votes",
-      data: [12, 19, 3, 5, 2, 3],
+      data: [
+        [12, 19],
+        [19, 3],
+        [3, 5],
+        [5, 2],
+        [2, 3],
+        [3, 9],
+        [9, 15],
+        [15, 10],
+      ],
       backgroundColor: [
         "rgba(255, 99, 132, 0.2)",
         "rgba(54, 162, 235, 0.2)",
@@ -1497,36 +1576,10 @@ const data = {
         "rgba(153, 102, 255, 1)",
         "rgba(255, 159, 64, 1)",
       ],
-      borderWidth: 1,
+      borderWidth: 3,
+      borderSkipped: false,
     },
   ],
-};
-
-const chartAreaBorder = {
-  id: "chartAreaBorder",
-  beforeDraw(
-    chart: {
-      ctx: any;
-      chartArea: { top: number; bottom: number; left: number; right: number; width: number; height: number };
-    },
-    _args: any,
-    _options: any
-  ) {
-    const {
-      ctx,
-      chartArea: { top, bottom, left, right, width, height },
-    } = chart;
-
-    // console.log({ top });
-    // console.log({ bottom });
-    // console.log({ _args });
-    // console.log({ _options });
-
-    ctx.strokeStyle = "black";
-    ctx.strokeRect(right - 1, top, 0, height);
-    ctx.strokeStyle = "red";
-    ctx.strokeRect(left, top, width - 1, 0);
-  },
 };
 
 const config = {
@@ -1539,7 +1592,6 @@ const config = {
       },
     },
   },
-  plugins: [chartAreaBorder],
 };
 
 new Chart(document.getElementById("myChart") as HTMLCanvasElement, config as unknown as ChartConfiguration);
