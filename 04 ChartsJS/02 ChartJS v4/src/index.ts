bfdcba20ef648c6dcf66089821a1f3d1b6cfd857
Michal Radomski <michalra@op.pl>
2023-06-17 23:49:42 +0200

    ChartJS v4 - initial commit2

--- 04 ChartsJS/02 ChartJS v4/src/index.ts
+++ 04 ChartsJS/02 ChartJS v4/src/index.ts
@@ -3,49 +3,38 @@ import { ChartConfiguration } from "chart.js";
 const { Chart } = window;
 
 //* Default Code
-const ctx = document.getElementById("myChart") as HTMLCanvasElement;
-
 const labels = ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"];
 
-// const data = {
-//   labels: labels,
-//   datasets: [
-//     {
-//       label: "# of Votes",
-//       data: [12, 19, 3, 5, 2, 3],
-//       backgroundColor: [
-//         "rgba(255, 99, 132, 0.2)",
-//         "rgba(54, 162, 235, 0.2)",
-//         "rgba(255, 206, 86, 0.2)",
-//         "rgba(75, 192, 192, 0.2)",
-//         "rgba(153, 102, 255, 0.2)",
-//         "rgba(255, 159, 64, 0.2)",
-//       ],
-//       borderColor: [
-//         "rgba(255, 99, 132, 1)",
-//         "rgba(54, 162, 235, 1)",
-//         "rgba(255, 206, 86, 1)",
-//         "rgba(75, 192, 192, 1)",
-//         "rgba(153, 102, 255, 1)",
-//         "rgba(255, 159, 64, 1)",
-//       ],
-//       borderWidth: 3,
-//     },
-//   ],
-// };
+const data = {
+  labels: labels,
+  datasets: [
+    {
+      label: "# of Votes",
+      data: [12, 19, 3, 5, 2, 3],
+      backgroundColor: [
+        "rgba(255, 99, 132, 0.2)",
+        "rgba(54, 162, 235, 0.2)",
+        "rgba(255, 206, 86, 0.2)",
+        "rgba(75, 192, 192, 0.2)",
+        "rgba(153, 102, 255, 0.2)",
+        "rgba(255, 159, 64, 0.2)",
+      ],
+      borderColor: [
+        "rgba(255, 99, 132, 1)",
+        "rgba(54, 162, 235, 1)",
+        "rgba(255, 206, 86, 1)",
+        "rgba(75, 192, 192, 1)",
+        "rgba(153, 102, 255, 1)",
+        "rgba(255, 159, 64, 1)",
+      ],
+      borderWidth: 3,
+    },
+  ],
+};
 
 const config = {
   type: "bar",
-  data: {
-    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-    datasets: [
-      {
-        label: "# of Votes",
-        data: [12, 19, 3, 5, 2, 3],
-        borderWidth: 3,
-      },
-    ],
-  },
+  data: data,
   options: {
     scales: {
       y: {
@@ -55,4 +44,4 @@ const config = {
   },
 };
 
-new Chart(ctx, config as ChartConfiguration);
+new Chart(document.getElementById("myChart") as HTMLCanvasElement, config as ChartConfiguration);
