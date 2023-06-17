e815020e878549bc98a9048d367eb3f84930cdaa
Michal Radomski <michalra@op.pl>
2023-06-18 00:05:33 +0200

    Course - commit44

--- 04 ChartsJS/02 ChartJS v4/src/index.ts
+++ 04 ChartsJS/02 ChartJS v4/src/index.ts
@@ -2,46 +2,98 @@ import { ChartConfiguration } from "chart.js";
 
 const { Chart } = window;
 
-//* Default Code
-const labels = ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"];
-
+//* Radial Bar Chart
 const data = {
-  labels: labels,
   datasets: [
     {
-      label: "# of Votes",
-      data: [12, 19, 3, 5, 2, 3],
-      backgroundColor: [
-        "rgba(255, 99, 132, 0.2)",
-        "rgba(54, 162, 235, 0.2)",
-        "rgba(255, 206, 86, 0.2)",
-        "rgba(75, 192, 192, 0.2)",
-        "rgba(153, 102, 255, 0.2)",
-        "rgba(255, 159, 64, 0.2)",
-      ],
-      borderColor: [
-        "rgba(255, 99, 132, 1)",
-        "rgba(54, 162, 235, 1)",
-        "rgba(255, 206, 86, 1)",
-        "rgba(75, 192, 192, 1)",
-        "rgba(153, 102, 255, 1)",
-        "rgba(255, 159, 64, 1)",
-      ],
+      label: "Red",
+      data: [12],
+      backgroundColor: ["rgba(255, 99, 132, 0.2)"],
+      borderColor: ["rgba(255, 99, 132, 1)"],
       borderWidth: 3,
+      circumference: (ctx: { dataset: { data: number[] } }) => {
+        return (ctx.dataset.data[0] / 12) * 270;
+      },
+    },
+    {
+      label: "Blue",
+      data: [9],
+      backgroundColor: ["rgba(54, 162, 235, 0.2)"],
+      borderColor: ["rgba(54, 162, 235, 1)"],
+      borderWidth: 3,
+      circumference: (ctx: { dataset: { data: number[] } }) => {
+        return (ctx.dataset.data[0] / 12) * 270;
+      },
+    },
+    {
+      label: "Yellow",
+      data: [3],
+      backgroundColor: ["rgba(255, 159, 64, 0.2)"],
+      borderColor: ["rgba(255, 159, 64, 1)"],
+      borderWidth: 3,
+      circumference: (ctx: { dataset: { data: number[] } }) => {
+        return (ctx.dataset.data[0] / 12) * 270;
+      },
     },
   ],
 };
 
 const config = {
-  type: "bar",
+  type: "doughnut",
   data: data,
   options: {
-    scales: {
-      y: {
-        beginAtZero: true,
+    plugins: {
+      legend: {
+        display: false,
       },
     },
   },
 };
 
 new Chart(document.getElementById("myChart") as HTMLCanvasElement, config as ChartConfiguration);
+
+//- -------------------------------------------------
+
+//* Default Code
+// const labels = ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"];
+
+// const data = {
+//   labels: labels,
+//   datasets: [
+//     {
+//       label: "# of Votes",
+//       data: [12, 19, 3, 5, 2, 3],
+//       backgroundColor: [
+//         "rgba(255, 99, 132, 0.2)",
+//         "rgba(54, 162, 235, 0.2)",
+//         "rgba(255, 206, 86, 0.2)",
+//         "rgba(75, 192, 192, 0.2)",
+//         "rgba(153, 102, 255, 0.2)",
+//         "rgba(255, 159, 64, 0.2)",
+//       ],
+//       borderColor: [
+//         "rgba(255, 99, 132, 1)",
+//         "rgba(54, 162, 235, 1)",
+//         "rgba(255, 206, 86, 1)",
+//         "rgba(75, 192, 192, 1)",
+//         "rgba(153, 102, 255, 1)",
+//         "rgba(255, 159, 64, 1)",
+//       ],
+//       borderWidth: 3,
+//     },
+//   ],
+// };
+
+// const config = {
+//   type: "bar",
+//   data: data,
+//   options: {
+//     scales: {
+//       y: {
+//         beginAtZero: true,
+//       },
+//     },
+//   },
+// };
+
+// new Chart(document.getElementById("myChart") as HTMLCanvasElement, config as ChartConfiguration);
