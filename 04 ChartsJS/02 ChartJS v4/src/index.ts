709d46e74d46b459586067e37f7208ffe06145fd
Michal Radomski <michalra@op.pl>
2023-06-18 14:27:05 +0200

    Course - commit45f

--- 04 ChartsJS/02 ChartJS v4/src/index.ts
+++ 04 ChartsJS/02 ChartJS v4/src/index.ts
@@ -243,18 +243,18 @@ const noData = {
 
     data.datasets[0].data.forEach((datapoint: null | number, index: number) => {
       const angle = Math.PI / 180;
+      // console.log({ angle });
       ctx.translate(0, 0);
 
       if (datapoint === null) {
         ctx.fillStyle = "rgba(102, 102, 102, 0.2)";
         ctx.fillRect(x.getPixelForValue(index) - segment / 2, top, segment, height);
 
-        // ctx.rotate(90 * angle);
+        ctx.rotate(90 * angle);
         ctx.font = "bold 20px sans-serif";
         ctx.fillStyle = "dimgray";
-        ctx.fillText("No Data", x.getPixelForValue(index), height / 2);
-
-        // ctx.rotate(-90 * angle);
+        ctx.fillText("No Data", height / 2, -x.getPixelForValue(index));
+        ctx.rotate(-90 * angle);
         ctx.restore();
       }
     });
