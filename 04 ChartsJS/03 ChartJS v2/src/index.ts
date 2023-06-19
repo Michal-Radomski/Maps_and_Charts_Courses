2a07697055477a99df112ed6769b675eff24e96c
Michal Radomski <michalra@op.pl>
2023-06-19 02:33:16 +0200

    ChartJS v2 - initial commit2

--- 04 ChartsJS/03 ChartJS v2/src/index.ts
+++ 04 ChartsJS/03 ChartJS v2/src/index.ts
@@ -1,3 +1,47 @@
 import { ChartConfiguration } from "chart.js";
 
 const { Chart } = window;
+
+//* Default Code
+const labels = ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"];
+
+const data = {
+  labels: labels,
+  datasets: [
+    {
+      label: "# of Votes",
+      data: [12, 19, 3, 5, 2, 3],
+      backgroundColor: [
+        "rgba(255, 99, 132, 0.2)",
+        "rgba(54, 162, 235, 0.2)",
+        "rgba(255, 206, 86, 0.2)",
+        "rgba(75, 192, 192, 0.2)",
+        "rgba(153, 102, 255, 0.2)",
+        "rgba(255, 159, 64, 0.2)",
+      ],
+      borderColor: [
+        "rgba(255, 99, 132, 1)",
+        "rgba(54, 162, 235, 1)",
+        "rgba(255, 206, 86, 1)",
+        "rgba(75, 192, 192, 1)",
+        "rgba(153, 102, 255, 1)",
+        "rgba(255, 159, 64, 1)",
+      ],
+      borderWidth: 3,
+    },
+  ],
+};
+
+const config = {
+  type: "bar",
+  data: data,
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+};
+
+new Chart(document.getElementById("myChart") as HTMLCanvasElement, config as ChartConfiguration);
