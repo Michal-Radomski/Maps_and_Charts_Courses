5419817f067218e7fd3082f6b42c03b8dd8a37d4
Michal Radomski <michalra@op.pl>
2023-06-20 23:02:38 +0200

    Course - commit49

--- 04 ChartsJS/03 ChartJS v2/src/index.ts
+++ 04 ChartsJS/03 ChartJS v2/src/index.ts
@@ -2,46 +2,78 @@ import { ChartConfiguration } from "chart.js";
 
 const { Chart } = window;
 
-//* Default Code
-const labels = ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"];
+//* Line Chart
+const ctx = (document.getElementById("myChart")! as HTMLCanvasElement).getContext("2d");
+new Chart(ctx!, {
+  type: "line",
+  data: {
+    // @ts-ignore
+    xAxisID: "un",
+    labels: ["Jan", "Feb", "March", "April", "May", "June", "July", "August"],
+    datasets: [
+      {
+        label: "My sales",
+        backgroundColor: "rgba(255, 99, 132, 1)",
+        borderColor: "blue",
 
-const data = {
-  labels: labels,
-  datasets: [
-    {
-      label: "# of Votes",
-      data: [12, 19, 3, 5, 2, 3],
-      backgroundColor: [
-        "rgba(255, 99, 132, 0.2)",
-        "rgba(54, 162, 235, 0.2)",
-        "rgba(255, 206, 86, 0.2)",
-        "rgba(75, 192, 192, 0.2)",
-        "rgba(153, 102, 255, 0.2)",
-        "rgba(255, 159, 64, 0.2)",
-      ],
-      borderColor: [
-        "rgba(255, 99, 132, 1)",
-        "rgba(54, 162, 235, 1)",
-        "rgba(255, 206, 86, 1)",
-        "rgba(75, 192, 192, 1)",
-        "rgba(153, 102, 255, 1)",
-        "rgba(255, 159, 64, 1)",
-      ],
-      borderWidth: 3,
-    },
-  ],
-};
+        data: [0, 20, 5, 2, 20, 30, 40, 20],
+      },
+    ],
+  },
 
-const config = {
-  type: "bar",
-  data: data,
   options: {
     scales: {
-      y: {
-        beginAtZero: true,
-      },
+      yAxes: [
+        {
+          stacked: true,
+        },
+      ],
     },
-  },
-};
+  } as ChartConfiguration,
+});
 
-new Chart(document.getElementById("myChart") as HTMLCanvasElement, config as ChartConfiguration);
+//- ----------------------------------------------
+
+//* Default Code
+// const labels = ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"];
+
+// const data = {
+//   labels: labels,
+//   datasets: [
+//     {
+//       label: "# of Votes",
+//       data: [12, 19, 3, 5, 2, 3],
+//       backgroundColor: [
+//         "rgba(255, 99, 132, 0.2)",
+//         "rgba(54, 162, 235, 0.2)",
+//         "rgba(255, 206, 86, 0.2)",
+//         "rgba(75, 192, 192, 0.2)",
+//         "rgba(153, 102, 255, 0.2)",
+//         "rgba(255, 159, 64, 0.2)",
+//       ],
+//       borderColor: [
+//         "rgba(255, 99, 132, 1)",
+//         "rgba(54, 162, 235, 1)",
+//         "rgba(255, 206, 86, 1)",
+//         "rgba(75, 192, 192, 1)",
+//         "rgba(153, 102, 255, 1)",
+//         "rgba(255, 159, 64, 1)",
+//       ],
+//       borderWidth: 3,
+//     },
+//   ],
+// };
+
+// const config = {
+//   type: "bar",
+//   data: data,
+//   options: {
+//     scales: {
+//       y: {
+//         beginAtZero: true,
+//       },
+//     },
+//   },
+// };
+
+// new Chart(document.getElementById("myChart") as HTMLCanvasElement, config as ChartConfiguration);
