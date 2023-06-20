44abefb0afdf0124385d0133c73156b3352a3202
Michal Radomski <michalra@op.pl>
2023-06-20 23:49:41 +0200

    Course - commit50a

--- 04 ChartsJS/03 ChartJS v2/src/index.ts
+++ 04 ChartsJS/03 ChartJS v2/src/index.ts
@@ -59,48 +59,53 @@ const { Chart } = window;
 // });
 
 //* Bar Chart
-const labels = ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"];
+const ctx = (document.getElementById("myChart")! as HTMLCanvasElement).getContext("2d");
+new Chart(ctx!, {
+  // type: "bar",
+  type: "horizontalBar",
 
-const data = {
-  labels: labels,
-  datasets: [
-    {
-      label: "# of Votes",
-      data: [12, 19, 3, 5, 2, 3],
-      backgroundColor: [
-        "rgba(255, 99, 132, 0.2)",
-        "rgba(54, 162, 235, 0.2)",
-        "rgba(255, 206, 86, 0.2)",
-        "rgba(75, 192, 192, 0.2)",
-        "rgba(153, 102, 255, 0.2)",
-        "rgba(255, 159, 64, 0.2)",
-      ],
-      borderColor: [
-        "rgba(255, 99, 132, 1)",
-        "rgba(54, 162, 235, 1)",
-        "rgba(255, 206, 86, 1)",
-        "rgba(75, 192, 192, 1)",
-        "rgba(153, 102, 255, 1)",
-        "rgba(255, 159, 64, 1)",
-      ],
-      borderWidth: 3,
-    },
-  ],
-};
+  data: {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+      {
+        label: "Monthly Sales",
+        backgroundColor: "red",
+        borderColor: "black",
+        borderWidth: 2,
+        // borderSkipped: "right",
+        // hoverBackgroundColor: "#000",
+      },
+      {
+        label: "Monthly Profits",
+        backgroundColor: "green",
+        borderColor: "black",
+        data: [20, 10, 5, 2, 20, 30, 45],
+      },
+    ],
+  },
 
-const config = {
-  type: "bar",
-  data: data,
   options: {
     scales: {
-      y: {
-        beginAtZero: true,
-      },
+      xAxes: [
+        {
+          //barPercentage: 1,
+          //categoryPercentage: 1,
+          barThickness: 40,
+          maxBarThickness: 30,
+          gridLines: {
+            offsetGridLines: false,
+          },
+          stacked: true,
+        },
+      ],
+      yAxes: [
+        {
+          stacked: true,
+        },
+      ],
     },
-  },
-};
-
-new Chart(document.getElementById("myChart") as HTMLCanvasElement, config as ChartConfiguration);
+  } as ChartConfiguration,
+});
 
 //- ----------------------------------------------
 
