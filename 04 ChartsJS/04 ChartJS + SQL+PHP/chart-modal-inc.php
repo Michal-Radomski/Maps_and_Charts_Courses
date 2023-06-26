161b79776329632107ad6f368e61b18b4de1b59e
Michal Radomski <michalra@op.pl>
2023-06-26 02:44:29 +0200

    Course - commit65e

--- 04 ChartsJS/04 ChartJS + SQL+PHP/chart-modal-inc.php
+++ 04 ChartsJS/04 ChartJS + SQL+PHP/chart-modal-inc.php
@@ -17,11 +17,51 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "INSERT INTO chartjs (chartid, charttype, chartdata, chartlabel, chartbackgroundcolor, chartbordercolor, chartlabels)
-VALUES (4,'line', $value, 'Additional', 'maroon', 'maroon', 'Chart_3')";
+$sql = "INSERT INTO chartjs (charttype, chartdata, chartlabel, chartbackgroundcolor, chartbordercolor, chartlabels)
+VALUES ('line', '$value', 'Additional', 'maroon', 'maroon', 'Chart_3')";
 
 if ($conn->query($sql) === TRUE) {
    echo "New record created successfully";
+
+
+
+
+
+
+
+
+
+   $conn = new mysqli($dbservername, $dbusername, $dbpassword, $dbname);
+   // Check connection
+   if ($conn->connect_error) {
+     die("Connection failed: " . $conn->connect_error);
+   }
+   $sql = "SELECT * FROM chartjs";
+   $result = $conn->query($sql);
+   
+   if ($result->num_rows > 0) {
+    
+     while($row = $result->fetch_assoc()) {
+       echo "id: " . $row["chartid"]. " - ChartData: " . $row["chartdata"]."<br>";
+     }
+   } else {
+     echo "0 results";
+   }
+   $conn->close();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   ?> 
  
   <div class="alert alert-success" id="alert_success">Update Success</div>
