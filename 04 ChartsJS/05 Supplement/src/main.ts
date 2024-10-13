e17b14c1615f17ceb293a05aeed6bb39aa8ab346
Michal Radomski <michalra@op.pl>
2024-10-13 20:19:38 +0200

    Chart.js course - supplement commit_5 added

--- 04 ChartsJS/05 Supplement/src/main.ts
+++ 04 ChartsJS/05 Supplement/src/main.ts
@@ -152,13 +152,82 @@ Chart.register(...registerables);
 // // console.log("Chart?.version:", Chart?.version);
 
 //* Example 2
+// // Data block
+// const data = {
+//   labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+//   datasets: [
+//     {
+//       label: "# of Votes",
+//       data: [12, 19, 3, 5, 2, 3],
+//       backgroundColor: [
+//         "rgba(255, 99, 132, 0.2)",
+//         "rgba(54, 162, 235, 0.2)",
+//         "rgba(255, 206, 86, 0.2)",
+//         "rgba(75, 192, 192, 0.2)",
+//         "rgba(153, 102, 255, 0.2)",
+//         "rgba(255, 159, 64, 0.2)",
+//       ],
+//       borderColor: [
+//         "rgba(255, 99, 132, 1)",
+//         "rgba(54, 162, 235, 1)",
+//         "rgba(255, 206, 86, 1)",
+//         "rgba(75, 192, 192, 1)",
+//         "rgba(153, 102, 255, 1)",
+//         "rgba(255, 159, 64, 1)",
+//       ],
+//       borderWidth: 1,
+//     },
+//   ],
+// };
+
+// // Config block
+// const config = {
+//   type: "bar",
+//   data: data,
+//   options: {
+//     scales: {
+//       x: {
+//         grid: {
+//           display: false,
+//           drawOnChartArea: false,
+//           drawTicks: false,
+//         },
+//         border: {
+//           display: false,
+//         },
+//       },
+//       y: {
+//         beginAtZero: true,
+//         grid: {
+//           dipl: false,
+//           drawOnChartArea: false,
+//           drawTicks: false,
+//         },
+//         border: {
+//           display: false,
+//         },
+//       },
+//     },
+//   },
+// } as ChartConfiguration;
+
+// // Init render block
+// new Chart(document.getElementById("myChart") as HTMLCanvasElement, config);
+
+// // Instantly assign Chart.js version
+// const chartVersion = document.getElementById("chartVersion") as HTMLSpanElement;
+
+// chartVersion.innerText = Chart.version;
+// // console.log("Chart?.version:", Chart?.version);
+
+//* Example 3
 // Data block
 const data = {
   labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
   datasets: [
     {
       label: "# of Votes",
-      data: [12, 19, 3, 5, 2, 3],
+      data: [12, 15, 3, 6, 9, 18],
       backgroundColor: [
         "rgba(255, 99, 132, 0.2)",
         "rgba(54, 162, 235, 0.2)",
@@ -176,9 +245,30 @@ const data = {
         "rgba(255, 159, 64, 1)",
       ],
       borderWidth: 1,
+      // barPercentage: 0.5,
+      // categoryPercentage: 0.8,
     },
   ],
 };
+
+// barThickness plugin
+const barThickness = {
+  id: "barThickness",
+  beforeDatasetsDraw(chart: { getDatasetMeta?: any; data?: any }) {
+    const { data } = chart;
+
+    const max = Math.max(...data.datasets[0].data);
+    const indexNum = data.datasets[0].data.indexOf(max);
+    const barWidth = chart.getDatasetMeta(0).data[indexNum].width;
+    // console.log({ max, indexNum, barWidth });
+
+    chart.getDatasetMeta(0).data.forEach((dataPoint: { width: number }, index: number) => {
+      const newWidth = (barWidth / max) * data.datasets[0].data[index];
+      dataPoint.width = newWidth;
+    });
+  },
+};
+// console.log("barThickness:", barThickness);
 
 // Config block
 const config = {
@@ -186,29 +276,12 @@ const config = {
   data: data,
   options: {
     scales: {
-      x: {
-        grid: {
-          display: false,
-          drawOnChartArea: false,
-          drawTicks: false,
-        },
-        border: {
-          display: false,
-        },
-      },
       y: {
         beginAtZero: true,
-        grid: {
-          dipl: false,
-          drawOnChartArea: false,
-          drawTicks: false,
-        },
-        border: {
-          display: false,
-        },
       },
     },
   },
+  plugins: [barThickness],
 } as ChartConfiguration;
 
 // Init render block
