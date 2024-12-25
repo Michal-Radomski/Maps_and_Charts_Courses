24bbf1aad071051848cbb5cf7be868a8d47b49f0
Michal Radomski <michalra@op.pl>
2024-12-25 23:18:05 +0100

    OpenLayers course - commit 02

--- 05 OpenLayers/01/src/main.ts
+++ 05 OpenLayers/01/src/main.ts
@@ -1,3 +1,21 @@
+import Map from "ol/Map";
+import OSM from "ol/source/OSM";
+import TileLayer from "ol/layer/Tile";
+import View from "ol/View";
+
 import "./style.scss";
 
-console.log("Vite testing");
+const map = new Map({
+  target: "map",
+  layers: [
+    new TileLayer({
+      source: new OSM(),
+    }),
+  ],
+  view: new View({
+    center: [0, 0],
+    zoom: 3,
+  }),
+});
+
+console.log("map:", map);
