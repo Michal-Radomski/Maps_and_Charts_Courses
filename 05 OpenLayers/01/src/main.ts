891866a3cb8250cdcbd82fc850b33b2b33f01787
Michal Radomski <michalra@op.pl>
2024-12-26 21:10:36 +0100

    OpenLayers course - commit 04

--- 05 OpenLayers/01/src/main.ts
+++ 05 OpenLayers/01/src/main.ts
@@ -2,11 +2,16 @@ import Map from "ol/Map";
 import OSM from "ol/source/OSM";
 import TileLayer from "ol/layer/Tile";
 import View from "ol/View";
+import { fromLonLat } from "ol/proj";
 
 import "./style.scss";
 
 // const init = (): void => console.log("init");
 // window.onload = init;
+
+const gdanskCoordinates = [18.645278, 54.3475]; // Longitude first
+const gdanskWebMercator = fromLonLat(gdanskCoordinates); // Convert to Web Mercator
+// console.log("gdanskWebMercator:", gdanskWebMercator);
 
 const map: Map = new Map({
   target: "map",
@@ -16,8 +21,13 @@ const map: Map = new Map({
     }),
   ],
   view: new View({
-    center: [0, 0],
-    zoom: 3,
+    // center: [0, 0],
+
+    center: gdanskWebMercator,
+    zoom: 10,
+    // rotation: 3.14 / 2, // 90deg
+    maxZoom: 16,
+    minZoom: 2,
   }),
 });
 console.log("map:", map);
