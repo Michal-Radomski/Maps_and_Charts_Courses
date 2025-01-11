a146b01fb33e02815adf0bd00517f15cfaad2b77
Michal Radomski <michalra@op.pl>
2025-01-12 00:21:04 +0100

    OpenLayers course - commit 40

--- 05 OpenLayers/04_proj/src/main.ts
+++ 05 OpenLayers/04_proj/src/main.ts
@@ -1,1 +1,21 @@
-console.log("test");
+import Map from "ol/Map";
+import View from "ol/View";
+import TileLayer from "ol/layer/Tile";
+import XYZ from "ol/source/XYZ";
+
+import "./style.scss";
+
+new Map({
+  target: "map",
+  layers: [
+    new TileLayer({
+      source: new XYZ({
+        url: "https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+      }),
+    }),
+  ],
+  view: new View({
+    center: [0, 0],
+    zoom: 2,
+  }),
+});
